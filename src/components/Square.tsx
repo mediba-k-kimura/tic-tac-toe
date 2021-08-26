507e8aec54f7656323c822b2cd6f05da2b78d266
mediba-k-kimura <k-kimura@mediba.jp>
2021-08-26 14:42:13 +0900

    BoardとSquareをReact.FCに修正

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { ISquare } from '../interface';
 interface SquareProps {
-    value: ISquare;
-    onClick: () => void;
-  }
-  function Square(props: SquareProps) {
-    return (
-      <button className="square" onClick={props.onClick}>
-        {props.value}
-      </button>
-    );
-  }
+  value: ISquare;
+  onClick: () => void;
+}
+
+const Square: React.FC<SquareProps> = ({ value, onClick }) => {
+  return (
+    <button className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
+}
   export default Square;
