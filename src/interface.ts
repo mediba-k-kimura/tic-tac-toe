aa9672c09c04d3b90e867f3763963bd3ea20785a
mediba-k-kimura <k-kimura@mediba.jp>
2021-09-16 15:52:34 +0900

    型定義修正

--- src/interface.ts
+++ src/interface.ts
@@ -2,3 +2,12 @@ export type ISquare = 'X' | 'O' | null;
 export interface History {
   squares: ISquare[];
 }
+interface ClickSquareTypes{
+  type: "CLICK_SQUARE";
+  index: number;
+}
+interface JumpToPastTypes{
+  type: "JUMP_TO_PAST";
+  step: number;
+}
+export type ActionTypes = ClickSquareTypes | JumpToPastTypes;
