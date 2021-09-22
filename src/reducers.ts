2dfa3de908d7439a0df18f088f3688ce555ce9a5
mediba-k-kimura <k-kimura@mediba.jp>
2021-09-22 17:34:21 +0900

    コメント追加

--- src/reducers.ts
+++ src/reducers.ts
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 import { CLICK_SQUARE, JUMP_TO_PAST } from "./actions";
 import { ISquare, ActionTypes } from "./interface";
 
+// 勝者を算出する
 export function calculateWinner(squares: Array<ISquare>) {
     const lines = [
       [0, 1, 2],
@@ -13,6 +14,7 @@ export function calculateWinner(squares: Array<ISquare>) {
       [0, 4, 8],
       [2, 4, 6],
     ];
+    // 勝ちパターン満たしているプレイヤーを返す
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
