880dffc8d1a88f578d23f96e5594845787419691
mediba-k-kimura <k-kimura@mediba.jp>
2021-09-28 16:51:14 +0900

    Storeと接続

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 import Game from './components/Game'
 import './index.css';
 
-ReactDOM.render(<Game />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <Game />
+    </Provider>,
+    document.getElementById('root')
+);
