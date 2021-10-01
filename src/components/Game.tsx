50ec316bae2f1f8a3f5ddc4f7de1d0b0a4ad5dff
mediba-k-kimura <k-kimura@mediba.jp>
2021-10-01 15:13:42 +0900

    Gameコンポーネント修正

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,60 +1,34 @@
-import React, { useState, useEffect } from "react";
-import { ISquare, History } from '../interface'
+import { clickSquare, jumpToPast } from '../redux/actions'
+import { useAppSelector, useAppDispatch } from '../redux/hooks'
+import { calculateWinner } from '../utils'
 import Board from './Board'
 
-function useStatus(xIsNext: boolean, winner: ISquare) {
-  const [status, setStatus] = useState<string>("Next player: " + (xIsNext ? "X" : "O"));
-  useEffect(() => {
-    if(winner){
-      setStatus("Winner: " + winner);
-    }else{
-      setStatus("Next player: " + (xIsNext ? "X" : "O"));
-    }
-  }, [xIsNext, winner]);
-  return status;
-}
-
 const Game: React.FC = () => {
+  const selector = useAppSelector(state => state.game)
+  const dispatch = useAppDispatch()
 
 
-  const handleClick = (i: number) => {
-    const _history = history.slice(0, stepNumber + 1);
-    const current = _history[_history.length - 1];
-    const squares = current.squares.slice();
-
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = xIsNext ? "X" : "O";
-    setHistory(_history.concat([{ squares: squares }]));
-    setStepNumber(_history.length);
-    setXIsNext(!xIsNext);
-  }
-
-  const jumpTo = (step: number) => {
-    setStepNumber(step);
-    setXIsNext(step % 2 === 0);
-  }
-
-  const current = history[stepNumber];
+  const history = selector.history;
+  const current = history[selector.stepNumber];
   const winner = calculateWinner(current.squares);
+
   const moves = history.map((step, move) => {
     const desc = move ? "Go to move #" + move : "Go to game start";
     return (
       <li key={move}>
-        <button onClick={() => jumpTo(move)}>{desc}</button>
+        <button onClick={() => dispatch(jumpToPast(move))}>{desc}</button>
       </li>
     );
   });
 
-  const status = useStatus(xIsNext, winner);
+  const status = winner ? `Winner: ${winner}` : `Next player: ${(selector.xIsNext ? "X" : "O")}`
 
   return (
     <div className="game">
       <div className="game-board">
         <Board
           squares={current.squares}
-          handleClickSquares={(i: number) => handleClick(i)}
+          handleClickSquares={(i: number) => dispatch(clickSquare(i))}
         />
       </div>
       <div className="game-info">
